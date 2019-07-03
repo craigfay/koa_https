a3f9f055a1b7235cb4f92f8627816a7f467863a7
Craig Fay <craigfay1@gmail.com>
2019-07-03 11:12:45 -0500

    WIP introduce nginx

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,10 @@ FROM node:10
 
 WORKDIR /usr/src/app
 
-COPY package.json .
+COPY package*.json .
 
 COPY . .
 
 CMD npm install
 
-EXPOSE 80
-
-EXPOSE 443
+EXPOSE 8080
--- server.js
+++ server.js
@@ -1,49 +1,7 @@
-'use strict';
-
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-
 const Koa = require('koa');
 const server = new Koa();
 
-const acmeRouter = require('./acme-router.js');
-server.use(acmeRouter.routes())
-server.use(acmeRouter.allowedMethods());
-
-const {
-  DOMAIN,
-  HTTP_PORT,
-  HTTPS_PORT,
-} = process.env
-
-const ssl = {
-  key: fs.readFileSync('./ssl/privkey.pem', 'utf8').toString(),
-  cert: fs.readFileSync('./ssl/fullchain.pem', 'utf8').toString(),
-}
-
-const serverCallback = server.callback();
-
-try {
-  const httpServer = http.createServer(serverCallback);
-  httpServer.listen(HTTP_PORT, function(err) {
-    if (!!err) console.error('HTTP:', err);
-    else console.log(`HTTP OK: http://${DOMAIN}:${HTTP_PORT}`);
-  });
-}
-catch (ex) {
-  console.error('Failed to start HTTP server\n', ex);
-}
- 
-try {
-  const httpServer = https.createServer(ssl, serverCallback);
-  httpServer.listen(HTTPS_PORT, function(err) {
-    if (!!err) console.error('HTTPS:', err);
-    else console.log(`HTTPS OK: http://${DOMAIN}:${HTTPS_PORT}`);
-  });
-}
-catch (ex) {
-  console.error('Failed to start HTTPS server\n', ex);
-}
+const { PORT } = process.env;
+server.listen(PORT, () => console.log('Listening on port', PORT));
 
 module.exports = server;
