e5ccac7dbcef4472d2c152e2262acb12b239bea9
Craig Fay <craigfay1@gmail.com>
2019-07-06 16:34:50 -0500

    Add setup confirmation

--- setup.sh
+++ setup.sh
@@ -18,9 +18,12 @@ done
 echo "Obtaining SSL certificates ..."
 sudo docker-compose build
 sudo docker-compose up certbot || return 1
+sudo docker-compose down --volumes || return 1
 
 echo "Generating Diffie-Hellman key ..."
 sudo openssl dhparam -out ./dhparam/dhparam-2048.pem 2048 || return 1
 
 echo "Adding new nginx config ..."
 mv after-ssl.conf nginx-conf/nginx.conf || return 1
+
+echo "Done! use `sudo docker-compose up -d` to start the server."
