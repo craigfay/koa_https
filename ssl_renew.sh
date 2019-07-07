15cb79b8adb34214081d20655dc89cb637465725
Craig Fay <craigfay1@gmail.com>
2019-07-07 12:41:02 -0500

    Fix whitespace mistakes

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-declare docker_compose = "/usr/local/bin/docker-compose"
-declare yml_file = "/home/scarlet/koa_https/docker-compose.yml"
+declare docker_compose="/usr/local/bin/docker-compose"
+declare yml_file="/home/scarlet/koa_https/docker-compose.yml"
 
 # start certbot container and override default command
 $docker_compose -f $yml_file run certbot renew --dry-run \
