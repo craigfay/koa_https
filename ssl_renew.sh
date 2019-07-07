1ec0205d59d2f9f3d116c95bdad9bfcc10a309f0
Craig Fay <craigfay1@gmail.com>
2019-07-07 13:33:44 -0500

    Try without multiline command

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -4,5 +4,5 @@ declare yml_file="/home/scarlet/koa_https/docker-compose.yml"
 
 # start certbot container and override default command
 # send SIGHUP signal to webserver container to reload nginx config
-sudo $docker_compose -f $yml_file run certbot renew --dry-run \
-&& sudo $docker_compose -f $yml_file kill -s SIGHUP webserver
+sudo $docker_compose -f $yml_file run certbot renew --dry-run
+sudo $docker_compose -f $yml_file kill -s SIGHUP webserver
