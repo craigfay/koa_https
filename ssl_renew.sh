173a5b1fc0851393c43b753df95bff7640640cf4
Craig Fay <craigfay1@gmail.com>
2019-07-10 16:39:53 -0500

    Fix ssl renewal mistakes

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # intended to be triggered via cron
 declare docker_compose="/usr/local/bin/docker-compose"
-declare dev_yml_file="/home/scarlet/koa_https/docker-compose.yml"
+declare yml_file="/home/scarlet/koa_https/docker-compose.yml"
 declare prod_yml_file="/home/scarlet/koa_https/docker-compose.prod.yml"
 
 # start certbot container and override default command
 # send SIGHUP signal to webserver container to reload nginx config
-sudo $docker_compose -f $yml_file run certbot renew --dry-run
-sudo $docker_compose -f $yml_file kill -s SIGHUP webserver
+sudo $docker_compose -f $yml_file -f $prod_yml_file run certbot renew --dry-run
+sudo $docker_compose -f $yml_file -f $prod_yml_file kill -s SIGHUP webserver
