3f2653e9b11de28889d7e411515d166a923ef8f6
Craig Fay <craigfay1@gmail.com>
2019-07-11 20:11:36 +0000

    Serve static files

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const Koa = require('koa');
 const server = new Koa();
 
 const { PORT } = process.env;
-server.use(ctx => ctx.body = 'hello');
+const html = `
+	<link rel="stylesheet" href="/main.css">
+	<div>Hello from node</div>
+`;
+server.use(ctx => ctx.body = html);
 server.listen(PORT, () => console.log('Listening on port', PORT));
 
 module.exports = server;
