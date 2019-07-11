f2bbadcc6d7320474a353c13e29970268078ba64
Craig Fay <craigfay1@gmail.com>
2019-07-11 16:48:43 -0500

    Update docker destroy

--- docker-destroy.sh
+++ docker-destroy.sh
@@ -1,8 +1,2 @@
-# remove all containers
-sudo docker stop $(sudo docker ps -q)
-
-# remove all containers
-sudo docker rm $(sudo docker ps -aq)
-
-# remove all volumes
-sudo docker volume rm $(sudo docker volume ls -q)
+# force destroy all containers and their volumes
+sudo docker rm -fv $(sudo docker ps -aq)
