c73cbe211dcaef3f257874621130a4e1c287d8eb
Craig Fay <craigfay1@gmail.com>
2019-07-14 02:45:55 +0000

    Change references and folder name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -12,7 +12,7 @@ if [[ -z $domains ]]; then
 fi
 
 declare -a files=(
-  "data/nginx/app.conf"
+  "volumes/nginx/app.conf"
 )
 
 echo "Replacing files ..."
@@ -21,7 +21,7 @@ for file in "${files[@]}"; do
 done
 
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./volumes/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
