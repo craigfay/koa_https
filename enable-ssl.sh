e5000fbcb15f0a2d593bdb470d555ccd8aab2fd1
Craig Fay <craigfay1@gmail.com>
2019-07-15 15:12:07 -0500

    Accept args

--- enable-ssl.sh
+++ enable-ssl.sh
@@ -6,17 +6,43 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains="$1"
+# parse args
+while getopts ":d:e:s:" opt; do
+  case $opt in
+    d) domains="$OPTARG"
+    ;;
+    e) email="$OPTARG" || "" # Adding a valid address is strongly recommended
+    ;;
+    s) staging="$OPTARG" || 1 # Set to 1 if you're testing your setup to avoid hitting request limits
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+# prompt for args if not passed
 if [[ -z $domains ]]; then
   echo -n "domains: "
   read domains
 fi
+if [[ -z $email ]]; then
+  echo -n "email"
+  read email
+fi
+if [[ -z $staging ]]; then
+  echo -n "staging: (y/n)"
+  read staging
+fi
+
+# validate staging arg
+case "$staging" in
+  y|Y ) ;;
+  n|N ) ;;
+  * ) echo "invalid input";;
+esac
 
 rsa_key_size=4096
 certbot_path="./volumes/production/certbot"
-
-email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$certbot_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -74,7 +100,7 @@ case "$email" in
 esac
 
 # Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
+if [ $staging != "n" ]; then staging_arg="--staging"; fi
 
 sudo docker-compose run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
