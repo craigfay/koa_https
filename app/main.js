e77782c446f53205c2b0e4a6f8fd97a9e638bfb4
Craig Fay <craigfay1@gmail.com>
2019-10-04 20:33:58 -0500

    Use Dockerfile

--- app/main.js
+++
@@ -1,8 +0,0 @@
-// Start serving HTTP
-const server = require('./server');
-server.listen(process.env.PORT, () => console.log('listening ...'));
-
-// Respond to shutdown signals
-const shutdown = () => server.close(err => process.exit(err ? 1 : 0));
-process.on('SIGINT', shutdown);
-process.on('SIGTERM', shutdown);
